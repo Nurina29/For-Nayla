89daeed0f8271fbf94a7b7abf2fba405c48d8c44
Nurina Miftakhul Uyun <miftakhulnurina@gmail.com>
2026-07-10 15:32:32 +0700

    Update answer.js

--- js/answer.js
+++ js/answer.js
@@ -49,9 +49,9 @@ const futureLetters = [
 ];
   
 const galleryPhotos = [
-  { src: "/assets/photos/3.jpeg", alt: "Nayla bocil beneran" },
-  { src: "/assets/photos/2.jpg", alt: "Samwis 🥔+🥕" },
-  { src: "/assets/photos/4.jpg", alt: "Potret Umbi fams yang berbahagia" }
+  { src: "assets/photos/3.jpeg", alt: "Nayla bocil beneran" },
+  { src: "assets/photos/2.jpg", alt: "Samwis 🥔+🥕" },
+  { src: "assets/photos/4.jpg", alt: "Potret Umbi fams yang berbahagia" },
   { src: "https://i.pravatar.cc/300?img=40", alt: "Nayla lucu" },
   { src: "https://i.pravatar.cc/300?img=45", alt: "Nayla dan keluarga" },
   { src: "https://i.pravatar.cc/300?img=50", alt: "Nayla bahagia" }
