e2e271fa1dac8210b824586a07416b14d493d00f
Nurina Miftakhul Uyun <miftakhulnurina@gmail.com>
2026-07-10 19:12:31 +0700

    Update answer.js

--- js/answer.js
+++ js/answer.js
@@ -10,6 +10,14 @@ const firstThought = [
   {
     author: "🍠 Umbi Ubi Ungu",
     text: "Anak sulungku WKWKWKK. Dulu kamu protes nggak mau cuma dianggap praktikan, dan entah sejak kapan malah sekarang udah ku anggap adekku sendiri wkwk"
+  },
+  {
+    author: "Umbi Talas aka Vibi",
+    text: "kalau aku denger nama nayla ya, yang kebayang tu ibu peri, karena nayla charming dan se lembut ituu?!?! 😭. sempet mikir kek.. ni manusia klo jengkel selalu senyum kali yaa gapernah marahh?? 😭. well.. she is very beautiful, very kind, very smart, aduuhh gatau lagi deh gueee 😫😫😫"
+  },
+  {
+    author: "Chikaa",
+    text: "SENYUM SAMA RAMAHNYA NAYLA, social energynya juga yang banyak. ga pernah absen buat nyapa orang dan selalu senyum "
   }
 ];
 
@@ -23,6 +31,14 @@ const ifTheyWere = [
     author: "🍠 Umbi Ubi Ungu",
     title: "Berang-berang yang tiap jalan selalu bawa wortel favoritnya 🦦🥕",
     reason: "Soalnya berang-berang tuh lucu, aktif, social butterfly banget, terus suka nempel sama orang yang dia sayang. Nah itu tuh kamu banget. Mana tiap ketemu aku juga pasti excited bzir. Terus wortelnya yaa karena aku setuju aja sih sama statementmu kalo kamu itu wortel di umbi universe, nih gabisa dijelasin alasan benernya cuma banyak orang yg sependapat wkwk"
+  },
+  {
+    author: "Umbi Talas aka Vibi",
+    text: "Tapi ya selain ibu peri, kalau nayla jadi hewan, one thing that fits best is kelinci putih. KARENA AKHH LEMBUTT, BAIKK, dan suka makan wortel wkwkwkwk (nay kamu umbi wortel kan yaa). kalau jadi benda dia kayak boneka, kalau profesi dia cocok jadi dokter"
+  },
+  {
+    author: "Chikaa",
+    text: "BUNGA, soalnya Nayla tuh selalu seger bahkan kelas pagi sampe sore keliatan tetep seger dan cantikkk"
   }
 ];
 
@@ -34,6 +50,14 @@ const memories = [
   {
     author: "🍠 Umbi Ubi Ungu",
     text: "memori yang paling nempel sih waktu kamu malem-malem kirim revisian laprak yak dan mau gamau aku jg harus koreksi disaat itu juga karena gamau km nunggu lama, alhasil kamu jd orang pertama yang menamatkan revisian gamtek dan langsung acc semua WKWK (jujur masih kaget aja ada anak serajin dan seniat ini) terus waktu itu kamu pernah kirim long text ke aku habis UAP gamtek dan jujur banget waktu baca aku nangis lama, soalnya disitu aku kek ngerasa oh ternyata bisa loh aku bermanfaat bagi manusia lain, jadi terimakasih yakk adek 💙"
+  },
+  {
+    author: "Umbi Talas aka Vibi",
+    text: "satu hal yang aku inget dari nayy adalah pas kita sekelass yeyeyeyy. i'm very gratefull dipertemukan sama nay sejak semester 1 🤩. inget gaa pas berli kena cutter? selain aku yang nganter berli ke kamar mandii, nay dengan sigap langsung ambil betadine dan hansaplast trus nyamperin ke kamar mandi buat bantu obatin lukanyaaa 🧚 #ibuperi"
+  },
+  {
+    author: "Chikaa",
+    text: "banyak banget sih ceritanya sama nay, tapi aku selalu inget cerita kita ngerjain plo, ini berkenang sih nayy jujur"
   }
 ];
 
@@ -45,6 +69,14 @@ const futureLetters = [
   {
     author: "🍠 Umbi Ubi Ungu",
     text: "Makasiiee Nay udah menjadi sosok Nayla seperti hari ini. Tetap jadi Nay yang selalu punya aura bocil, gampang excited sama hal-hal kecil, dan bisa bikin orang-orang di sekitarmu ikut ketawa. Tapi di saat yang sama, jangan lupa kalau kamu juga boleh capek, boleh sedih, dan nggak harus selalu jadi orang yang kuat buat semua orang. Semisal lagi ada di posisi itu, jangan dipendem sendiri ya. Kamu bisa cerita ke Umbi Fams kok. Inget ya, banyak banget orang yang sayang sama Nayla secara tulus, termasuk aku. Kita semua nggak mau kalau Nay lagi sedih atau capek sendirian. Makasih juga karena udah jadi adik yang selalu bikin aku gemes, walaupun tiap ketemu kerjaannya nempel mulu padahal aku bukan orang yang suka di-physical touch WKWKWK. Tapi yaudah deh, khusus kamu aku kasih pengecualian, yang penting bisa bikin anak sulungku happy. Entah kamu sadar atau enggak, tapi kamu tuh salah satu orang yang selalu berhasil bawa energi positif ke sekitarmu. Makanya aku harap kamu tetap jadi orang yang baik, tapi jangan sampai kebaikan itu bikin orang lain semena-mena sama kamu. Kamu juga pantas dikelilingi orang-orang yang sayang, menghargai, dan menjaga kamu sebaik kamu selalu menjaga orang lain. Selamat ulang tahun yang ke-21 ya, Nay! Semoga di umur yang baru ini Allah selalu kasih kesehatan, kebahagiaan, hati yang tenang, dan langkah yang dimudahkan dalam setiap urusanmu. Semoga semua doa yang selama ini kamu simpan pelan-pelan dikabulkan di waktu terbaik menurut-Nya, semua usaha dan kerja kerasmu membuahkan hasil yang baik, kuliahnya lancar sampai lulus nanti, rezekinya dilapangkan, dan semoga kamu selalu dipertemukan dengan orang-orang yang benar-benar tulus menyayangimu. Aamiin. 💙 Pokoknya, tetap jadi berang-berang pembawa wortel favoritku ya WKWKWK. Jangan pernah kehilangan sisi bocilmu, tapi jangan lupa juga buat selalu menjaga diri sendiri. Happy 21st, anak sulungku! 💙"
+  },
+  {
+    author: "Umbi Talas aka Vibi",
+    text: "haloo nayy, ini vibi dari umbi talas wkwkwkwk (duh knp talas ya? maaf asbun tiba-tiba kepikiran talas karena 3M abis bikin tepung talas 😭). happy birthdayy yaa.. semoga panjang umur, sehat selalu, segala urusannya dipermudah, dilancarkan setiap jalannya, dan semogaa menjadi pribadi yang lebih baik untuk kedepannyaa. nayla manusia hebatt, manusia kuatt, liked and loved by everyone. harapanku buat nayy, tetep jadi orang baikk yaa, tetep jadi manusia yang selalu menebarkan kebaikan dan memberikan energi positif untuk orang orang sekitar. akuu tauu pasti jadi nayy ga se gampang dan se mudah keliatannya, nay pasti pernah ngerasa down, pernah ngerasa hidupnya berat dan terpuruk. tapi nayy satu hal yang kamu harus tau, you're the greatest person i've ever met, sekali lagi kamu manusia kuat, manusia hebat. duh aku ga kebayang dibalik senyum yang selalu kamu tunjukin ke orang orang, pasti pernah ada kesedihan yang gapernah kamu tunjukin. nayy kalau suatu hari nanti kamu merasa sedih dan gaada yang bisa ngertiin nayy, selalu ingat kalau nayy dikelilingi orang baik yang sayang sama nayy yaa, soo jangan pernah merasa sendiri. once again, happy birthday ❤️"
+  },
+  {
+    author: "Chikaa",
+    text: "NAY TETEP SENYUM YAAA, jangan banyak sedihnyaaa. semoga kebaikan selalu menyertai kamuuu"
   }
 ];
   
