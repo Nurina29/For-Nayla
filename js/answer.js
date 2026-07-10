f718fea3252e6e16ce9249ccb2d972014eacdead
Nurina Miftakhul Uyun <miftakhulnurina@gmail.com>
2026-07-10 19:27:17 +0700

    Update answer.js

--- js/answer.js
+++ js/answer.js
@@ -35,12 +35,12 @@ const ifTheyWere = [
   {
     author: "Umbi Talas aka Vibi",
     title: "Kelinci putih🐇",
-    text: "Tapi ya selain ibu peri, kalau nayla jadi hewan, one thing that fits best is kelinci putih. KARENA AKHH LEMBUTT, BAIKK, dan suka makan wortel wkwkwkwk (nay kamu umbi wortel kan yaa). kalau jadi benda dia kayak boneka, kalau profesi dia cocok jadi dokter"
+    reason: "Tapi ya selain ibu peri, kalau nayla jadi hewan, one thing that fits best is kelinci putih. KARENA AKHH LEMBUTT, BAIKK, dan suka makan wortel wkwkwkwk (nay kamu umbi wortel kan yaa). kalau jadi benda dia kayak boneka, kalau profesi dia cocok jadi dokter"
   },
   {
     author: "Chikaa",
     title: "BUNGA 💐🌸🏵🌹🌺🌻🌼🌷",
-    text: "soalnya Nayla tuh selalu seger bahkan kelas pagi sampe sore keliatan tetep seger dan cantikkk"
+    reason: "soalnya Nayla tuh selalu seger bahkan kelas pagi sampe sore keliatan tetep seger dan cantikkk"
   }
 ];
 
