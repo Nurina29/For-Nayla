6b3c780a705efe4333195bea1829294b99a32791
Nurina Miftakhul Uyun <miftakhulnurina@gmail.com>
2026-07-10 15:29:02 +0700

    Update answer.js

--- js/answer.js
+++ js/answer.js
@@ -47,9 +47,12 @@ const futureLetters = [
     text: "Dear Nayla, jangan pernah berubah jadi orang yang hangat dan tulus. Dunia butuh lebih banyak orang seperti kamu. Aku bangga bisa mengenalmu, dan aku yakin kamu akan jadi seseorang yang hebat di masa depan."
   }
 ];
-
+  
 const galleryPhotos = [
   { src: "/assets/photos/3.jpeg", alt: "Nayla bocil beneran" },
   { src: "/assets/photos/2.jpg", alt: "Samwis 🥔+🥕" },
   { src: "/assets/photos/4.jpg", alt: "Potret Umbi fams yang berbahagia" }
+  { src: "https://i.pravatar.cc/300?img=40", alt: "Nayla lucu" },
+  { src: "https://i.pravatar.cc/300?img=45", alt: "Nayla dan keluarga" },
+  { src: "https://i.pravatar.cc/300?img=50", alt: "Nayla bahagia" }
 ];
